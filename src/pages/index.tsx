98234cfa9bcb9bbb90e727db0ca795ac6d46b21a
jhw <a@b.c>
2025-02-05 12:18:41 +0100

    Add a single dynamic route, redirect and a link to reproduce the issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
+      <h1>Reproduce issue with "??"</h1>
+      <Link href={"/a"}>link to the problematic page</Link>
     </main>
   );
 }
