f7ad65e03e78fd9be1b01250766c195931429906
John Hagen <johnhagenmsc@gmail.com>
2025-02-06 15:32:35 +0100

    wip

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export const config = {
   matcher: [
     {
       source:
-        "/((?!images|svgs|icons|fonts|static|_next|manifest.json|favicon-*[32x32]*.*|[\\d\\w/.]*.css.*).*)",
+        "/((?!images|svgs|icons|fonts|favicon-*[32x32]*.*|[\\d\\w/.]*.css.*).*)",
     },
   ],
 };
